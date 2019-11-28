b37857dd1320b3da129557077656ccf96bb9290e
mattbrailsford <me@mattbrailsford.com>
2019-11-28 09:44:32 +0000

    Switched to using product information extractor decorator to generate variant names rather than storing the name on the variant node

---
+++ src/Vendr.DemoStore/Web/Extractors/ChildVariantUmbracoProductInformationExtractor.cs
@@ -0,0 +1,23 @@
+﻿using Umbraco.Web;
+using Vendr.Core.Models;
+using Vendr.Core.Services;
+using Vendr.Web.Extractors;
+
+namespace Vendr.DemoStore.Web.Extractors
+{
+    public class ChildVariantUmbracoProductInformationExtractor : UmbracoProductInformationExtractor
+    {
+        public ChildVariantUmbracoProductInformationExtractor(IUmbracoContextAccessor umbracoContextAccessor, VendrServiceContext vendrServices) 
+            : base(umbracoContextAccessor, vendrServices)
+        { }
+
+        public override IProductSnapshot GetProductSnapshot(string productReference, string languageIsoCode)
+        {
+            // Generatr the snapshot using the default extractor
+            var snapshot = (UmbracoProductSnapshot)base.GetProductSnapshot(productReference, languageIsoCode);
+                
+            // Decorate the snapshot with our custom decorator
+            return new ChildVariantUmbracoProductSnapshotDecorator(snapshot);
+        }
+    }
+}
---
+++ src/Vendr.DemoStore/Web/Extractors/ChildVariantUmbracoProductSnapshotDecorator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Vendr.Core.Models;
+using Vendr.DemoStore.Models;
+using Vendr.Web.Extractors;
+
+namespace Vendr.DemoStore.Web.Extractors
+{
+    /// <summary>
+    /// Snapshot decorator to allow us to manipulate the product name where the content type
+    /// is a variant product. In that situation we want to combine the variant product name
+    /// with it's parent product name.
+    /// </summary>
+    public class ChildVariantUmbracoProductSnapshotDecorator : ProductSnapshotBase
+    {
+        private readonly UmbracoProductSnapshot _snapshot;
+
+        public ChildVariantUmbracoProductSnapshotDecorator(UmbracoProductSnapshot snapshot)
+        {
+            _snapshot = snapshot;
+        }
+
+        public override string Name => _snapshot.Content.ContentType.Alias == ProductVariant.ModelTypeAlias
+            ? $"{_snapshot.Content.Parent.Name} - {_snapshot.Name}"
+            : _snapshot.Name;
+
+        #region Passthrough
+
+        public override Guid StoreId => _snapshot.StoreId;
+
+        public override string ProductReference => _snapshot.ProductReference;
+
+        public override string Sku => _snapshot.Sku;
+
+        public override Guid? TaxClassId => _snapshot.TaxClassId;
+
+        public override IEnumerable<ProductPrice> Prices => _snapshot.Prices;
+
+        public override IDictionary<string, string> Properties => _snapshot.Properties;
+
+        #endregion
+    }
+}
